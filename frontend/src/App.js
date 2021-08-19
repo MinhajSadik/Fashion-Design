b748f40063163c6af07df2ff4c58e7faa128374b
MinhajSadik <MinhajoSadik@gmail.com>
2021-08-19 08:07:03 +0600

    [adding] working and breaking changes

---
+++ frontend/src/App.js
@@ -0,0 +1,54 @@
+import { React } from 'react';
+import data from './data';
+function App() {
+  return (<div className="grid-container">
+  <header className="row">
+    <div>
+      <a className="brand" href="index.html">Fashion Design</a>
+    </div>
+    <div>
+      <a href="cart.html">Cart</a>
+      <a href="signin.html">Sign In</a>
+    </div>
+  </header>
+  <main>
+    <div className="row center">
+      {
+        data.products.map((product)=> (
+          <div key={product._id} className="card">
+        <a href={`/product/${product._id}`}>
+          <img className="medium" src="./images/p1.jpg" alt="product" />
+        </a>
+        <div className="card-body">
+          <a href="product.html">
+            <h2>{product.name}</h2>
+          </a>
+          <div className="rating">
+            <span>
+              <i className="fa fa-star"></i>
+            </span>
+            <span>
+              <i className="fa fa-star"></i>
+            </span>
+            <span>
+              <i className="fa fa-star"></i>
+            </span>
+            <span>
+              <i className="fa fa-star"></i>
+            </span>
+            <span>
+              <i className="fa fa-star"></i>
+            </span>
+          </div>
+          <div className="price">$120</div>
+        </div>
+      </div>
+        ))
+      }
+    </div>
+  </main>
+  <footer className="row center">All Right Reserved By MinhajSadik</footer>
+</div>);
+}
+
+export default App;
