b70614235e34484fb792bffde2c20ec31cb0a8b5
MinhajSadik <MinhajoSadik@gmail.com>
2021-08-20 19:05:09 +0600

    [start] working fashion design

--- frontend/src/Screens/ProductScreen.js
+++ frontend/src/Screens/ProductScreen.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import data from '../data';
 
-const ProductScreen = () => {
+const ProductScreen = (props) => {
+    const product = data.products.find((x) => x._id === props.match.params.id);
+    if(!product){
+        return <div> Products Not Found</div>;
+    }
     return (
-        <div>
-            Product Screen
+    <div>
+        <div className="row">
+            <div className="col-2">
+                <img className="large" src={product.image} alt="{product.name}"></img>
+            </div>
+            <div className="col-1">
+                
+            </div>
+            <div className="col-1">
+                
+            </div>
         </div>
+    </div>
     );
 };
 
