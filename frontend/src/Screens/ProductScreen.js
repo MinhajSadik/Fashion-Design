4ab4506c40253a7c77b1ac7f6f1edb01bdde64ee
MinhajSadik <MinhajoSadik@gmail.com>
2021-08-20 19:25:33 +0600

    [adding] product components

--- frontend/src/Screens/ProductScreen.js
+++ frontend/src/Screens/ProductScreen.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Rating from '../Components/Rating/Rating';
 import data from '../data';
 
 const ProductScreen = (props) => {
@@ -6,21 +7,52 @@ const ProductScreen = (props) => {
     if(!product){
         return <div> Products Not Found</div>;
     }
-    return (
-    <div>
-        <div className="row">
+    return (<div>
+        <div className="row top">
             <div className="col-2">
                 <img className="large" src={product.image} alt="{product.name}"></img>
             </div>
             <div className="col-1">
-                
+                <ul>
+                    <li>
+                        <h1>{product.name}</h1>
+                    </li>
+                    <li>
+                        <Rating>
+                            rating={product.rating} numReviews={product.numReviews}
+                        </Rating>
+                    </li>
+                    <li>
+                        Price: ${product.price}
+                    </li>
+                    <li>
+                        Description: <p>{product.description}</p>
+                    </li>
+                </ul>
             </div>
             <div className="col-1">
-                
+                <div className="card card-body">
+                    <ul>
+                        <li>
+                            <div className="row">
+                                <div>Price</div>
+                                <div className="price">${product.price}</div>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="row">
+                                <div>Status</div>
+                                <div>{product.countInStock > 0 ? (<span className="success">In Stock</span>):(<span className="error">Unavailable</span>)}</div>
+                            </div>
+                        </li>
+                        <li>
+                            <button className="primary block">Add to Cart</button>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
-    </div>
-    );
+    </div>);
 };
 
 export default ProductScreen;
